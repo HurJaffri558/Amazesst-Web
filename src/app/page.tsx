1d91fdfeca8a471df677dc283461eb55f889400a
HurJaffri558 <162042198+HurJaffri558@users.noreply.github.com>
2026-08-18 02:09:57 +0500

    Add LogoScroller component to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
       <Hero />
       <Packages />
       <StatsSection />
+      <LogoScroller />
       <CaseStudySection />
       <Features />
       <Process />
